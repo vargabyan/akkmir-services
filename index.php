3d9a6e46cc67e8e3015a6a4d82f1d0a851d764d9
vargabyan <vardan.gabrielyan.it@gmail.com>
2024-04-17 14:02:11 +0500

    #1

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Akkmir services</title>
+</head>
+<body>
+
+
+<header class="core-container header">
+    <a class="header_logo-wrapper" href="/">
+        <p class="header_logo-name">ВОЛЬТ</p>
+        <span>Сервисный центр</span>
+    </a>
+    <form action="" class="header_select-city-wrapper">
+        <p class="header_working-hours">С 8:00 до 21:00</p>
+        <label class="header_city_label">
+            <span class="header_city-name">Екатеринбург</span>
+            <select class="header_city_select" name="" id="">
+                <option value="Екатеринбург">Екатеринбург</option>
+            </select>
+        </label>
+    </form>
+    <nav class="header_nav">
+        <ul>
+            <li><a href="/services">Услуги автосервиса</a></li>
+            <li><a href="/about-company">о компании</a></li>
+            <li><a href="/contacts">Контакты</a></li>
+        </ul>
+    </nav>
+    <a class="header_tel" href="tel:+7343261-62-62">
+        +7 343 261-62-62
+        <span>Заказать звонок</span>
+    </a>
+</header>
+
+
+
+<footer class="footer-wrapper">
+    <div class="core-container footer">
+        <div class="footer_logo-and-city-wrapper">
+            <a class="footer_logo-wrapper" href="/">
+                <p class="footer_logo-name">ВОЛЬТ</p>
+                <span>Сервисный центр</span>
+            </a>
+            <form action="" class="footer_select-city-wrapper">
+                <label class="footer_city_label">
+                    <span class="footer_city-name">Екатеринбург</span>
+                    <select class="footer_city_select" name="" id="">
+                        <option value="Екатеринбург">Екатеринбург</option>
+                    </select>
+                </label>
+            </form>
+            <a class="footer_tel" href="tel:+7343261-62-62">
+                +7 343 261-62-62
+                <span>Заказать звонок</span>
+            </a>
+        </div>
+        <div class="footer_nav-wrapper">
+            <nav class="footer_nav">
+                <ul>
+                    <li><a href="/about-company">О компании</a></li>
+                    <li><a href="/contacts">Контакты</a></li>
+                </ul>
+            </nav>
+            <div class="footer_policy-wrapper">
+                <a class="footer_policy" href="/policy">Политика конфиденциальности</a>
+            </div>
+        </div>
+        <nav class="footer_services-wrapper">
+            <ul>
+                <li><p>Услуги автосервиса</p></li>
+                <li><a href="">Ремонт ходовой части</a></li>
+                <li><a href="">Услуги диагноста-автоэлектрика</a></li>
+                <li><a href="">Ремонт тормозной системы</a></li>
+                <li><a href="">Шиномонтаж</a></li>
+                <li><a href="">Ремонт рулевого управления</a></li>
+                <li><a href="">Зарядка и обслуживание аккумуляторов</a></li>
+                <li><a href="">Работы по техническому обслуживанию</a></li>
+                <li><a href="">Ремонт стартеров и генераторов</a></li>
+                <li><a href="">Регулировка углов установки колёс</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
+
+<script src="scripts.js"></script>
+</body>
+</html>
